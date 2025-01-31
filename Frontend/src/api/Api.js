6739bef0d3fe666997c28d9a01f579899cb500b6
Amit <jha41735@gmail.com>
2025-01-31 12:26:14 +0530

    g

--- Frontend/src/api/Api.js
+++ Frontend/src/api/Api.js
@@ -9,8 +9,8 @@ import axios from "axios";
 // });
 
 const Api = axios.create({
-  baseURL: "http://localhost:5000",
-  withCredentials: true, // Ensure cookies are sent
+  baseURL:process.env.REACT_APP_BACKEND_URL || "http://localhost:5000",
+  withCredentials: true,
   headers: {
     "Content-Type": "application/json",
     Authorization: `Bearer ${localStorage.getItem("token")}`, // Include JWT token
